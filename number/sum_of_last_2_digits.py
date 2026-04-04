e6d73b0202f7081f201cd33bb9513f0470005da0
prasanthidasari6925 <prasanthidasari2659@gmail.com>
2026-04-04 22:51:34 +0530

    Add files via upload

---
+++ number/sum_of_last_2_digits.py
@@ -0,0 +1,17 @@
+num1 = int(input())
+num2 = int(input())
+
+num1 = abs(num1)
+num2 = abs(num2)
+
+if num1 == 0 or num2 == 0:
+    print(0)
+else:
+    i = max(num1, num2)
+    
+    while True:
+        if i % num1 == 0 and i % num2 == 0:
+            break
+        i += max(num1, num2)
+    
+    print(i)
